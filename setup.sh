cf435d50ec08a88a0634976a7dbeda8220559401
Kushagra Kumar <kkushagra86@gmail.com>
2026-08-24 02:01:59 +0530

    feat(kitty): configure top tab bar with browser-style shortcuts and multi-pane splitting

--- setup.sh
+++ setup.sh
@@ -798,13 +798,32 @@ dynamic_background_opacity yes
 window_padding_width       14 16
 confirm_os_window_close    0
 
-# --- Cursor & Tab Bar ---
-cursor_shape            beam
-cursor_beam_thickness   1.8
-cursor_blink_interval   0.5
-tab_bar_edge            bottom
-tab_bar_style           powerline
-tab_powerline_style     slanted
+# --- Tab Bar Aesthetics (Always visible at TOP like browser/terminal tabs) ---
+tab_bar_edge        top
+tab_bar_style       powerline
+tab_powerline_style slanted
+tab_bar_min_tabs    1
+tab_title_template  " {index}: {title} "
+active_tab_font_style   bold
+inactive_tab_font_style normal
+
+# --- Tab & Window Keybindings ---
+map ctrl+shift+t new_tab
+map ctrl+t new_tab
+map ctrl+shift+w close_tab
+map ctrl+w close_tab
+map ctrl+tab next_tab
+map ctrl+shift+tab previous_tab
+map ctrl+shift+right next_tab
+map ctrl+shift+left previous_tab
+map alt+1 goto_tab 1
+map alt+2 goto_tab 2
+map alt+3 goto_tab 3
+map alt+4 goto_tab 4
+map alt+5 goto_tab 5
+map ctrl+shift+enter new_window
+map ctrl+shift+[ previous_window
+map ctrl+shift+] next_window
 
 # --- Audio & Shell ---
 enable_audio_bell no
